fddd9f092cc99e427efe67bb47bfc03623702e67
Jesse Riggins <jriggins@users.noreply.github.com>
2019-04-07 08:50:22 -0500

    wip: before change from pure

--- src/components/login.js
+++ src/components/login.js
@@ -2,21 +2,40 @@ import React from 'react';
 
 function Login(props) {
   const users = props.users || {};
+  const onLogin = props.onLogin || (() => null);
+  // let selectedUser = Object.values(users)[0];
+  let selectedUser = Object.values(users)[0];
 
-  function renderUsers(users) {
+  const renderUsers = (users, selectedUser) => {
     return Object.values(users).map((user) => (
-      <option key={user.id} className="user">{user.name}</option>
+      <option key={user.id} value={user.id} className="user">{user.name}</option>
     ));
   }
 
+  const getSelectedUser = (event) => {
+    return users[event.target.value];
+  }
+
+  const handleLogin = (event, selectedUser, callback) => {
+    console.log('User: ', selectedUser)
+    callback(selectedUser);
+  }
+
+  const handleUserSelect = (event) => {
+    // TODO: Is this internal state change bad?
+    console.log('User selected')
+    selectedUser = getSelectedUser(event);
+  }
+
+  console.log("Rendering: ", new Date().getTime())
   return (
     <div>
-      <form>
-        <select>
-          { renderUsers(users) }
+      <div>
+        <select data-testid="userSelect" onChange={handleUserSelect}>
+          { renderUsers(users, selectedUser) }
         </select>
-        <button>Sign In</button>
-      </form>
+        <button onClick={(event) => handleLogin(event, selectedUser, onLogin)}>Sign In</button>
+      </div>
     </div>
   );
 }
--- src/components/login.test.js
+++ src/components/login.test.js
@@ -1,28 +1,59 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 
 import Login from './login';
 
 afterEach(cleanup);
 
-it('has a list of users to select from', () => {
-  const users = {
-    sarahedo: {
-      id: 'sarahedo',
-      name: 'Sarah Edo',
-    },
-    tylermcginnis: {
-      id: 'tylermcginnis',
-      name: 'Tyler McGinnis',
-    },
-    johndoe: {
-      id: 'johndoe',
-      name: 'John Doe',
-    }
-  }
-  const { queryByText } = render(<Login users={users}/>);
+describe('Login', () => {
+  test('has a list of users to select from', () => {
+    const users = {
+      sarahedo: {
+        id: 'sarahedo',
+        name: 'Sarah Edo',
+      },
+      tylermcginnis: {
+        id: 'tylermcginnis',
+        name: 'Tyler McGinnis',
+      },
+      johndoe: {
+        id: 'johndoe',
+        name: 'John Doe',
+      }
+    };
+    const { getByText } = render(<Login users={users}/>);
 
-  expect(queryByText('Sarah Edo')).not.toBeNull();
-  expect(queryByText('Tyler McGinnis')).not.toBeNull();
-  expect(queryByText('John Doe')).not.toBeNull();
-})
+    expect(getByText('Sarah Edo')).not.toBeNull();
+    expect(getByText('Tyler McGinnis')).not.toBeNull();
+    expect(getByText('John Doe')).not.toBeNull();
+  });
+
+  test('logins in a user', () => {
+    const users = {
+      sarahedo: {
+        id: 'sarahedo',
+        name: 'Sarah Edo',
+      },
+      tylermcginnis: {
+        id: 'tylermcginnis',
+        name: 'Tyler McGinnis',
+      },
+      johndoe: {
+        id: 'johndoe',
+        name: 'John Doe',
+      }
+    };
+
+    const clickHandler = jest.fn(x => console.log("I have been called with: ", x));
+
+    const { getByDisplayValue, getByTestId, getByText, debug } = render(<Login users={users} onLogin={clickHandler}/>);
+
+    fireEvent.change(getByDisplayValue('Sarah Edo'), {target: {value: 'tylermcginnis'}});
+    getByDisplayValue('Tyler McGinnis')
+    fireEvent.click(getByText('Sign In'));
+
+    expect(clickHandler.mock.calls.length).toEqual(1);
+    debug(clickHandler.mock.calls[0])
+    expect(clickHandler.mock.calls[0][0].id).toEqual('tylermcginnis');
+  });
+});
