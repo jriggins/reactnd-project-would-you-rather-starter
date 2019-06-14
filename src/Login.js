a92ef551d23177ae66135477bf6679f6c30057dc
Jesse Riggins <jriggins@users.noreply.github.com>
2019-06-14 14:07:31 -0500

    wip: Made Login a Stateful Component

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
-const Login = ({users}) => {
-  return(
-    <div>
-      <form>
-        <label htmlFor="login_users">Login</label>
-        <select id="login_users">
-          <option data-testid="default">--- Please select a user ---</option>
-          {
-            Object.values(users || {}).map((user) => {
-              return <option key={user.id} data-testid={user.id}>{user.name}</option>
-            })
-          }
-        </select>
-      </form>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.users = props.users || {};
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="login_users">Login</label>
+          <select id="login_users">
+            <option data-testid="default">--- Please select a user ---</option>
+            {
+              Object.values(this.users).map((user) => {
+                return <option key={user.id} data-testid={user.id}>{user.name}</option>
+              })
+            }
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
