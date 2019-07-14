80e92538ab99c5fe03a86e6ea2a479d7420a00be
Jesse Riggins <jriggins@users.noreply.github.com>
2019-07-14 16:25:12 -0500

    Convert Leaderboard to a functional component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class Leaderboard extends React.Component {
-  render() {
-    const { leaderboard } = this.props;
-
-    return (
-      <div>
-        <p>Leaderboard</p>
-        <ul>
-          {leaderboard.map((leaderboardItem) => (
-            <li key={leaderboardItem.id}>
-              <div>
-                <p>{leaderboardItem.name}</p>
-                <p>
-                  <img alt={`${leaderboardItem.name}'s Avatar`} className="avatar" src={leaderboardItem.avatarURL} />
-                </p>
-                <p>{leaderboardItem.answerCount}</p>
-                <p>{leaderboardItem.questionCount}</p>
-                <p>{leaderboardItem.score}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function Leaderboard({ leaderboard }) {
+  return (
+    <div>
+      <p>Leaderboard</p>
+      <ul>
+        {leaderboard.map((leaderboardItem) => (
+          <li key={leaderboardItem.id}>
+            <div>
+              <p>{leaderboardItem.name}</p>
+              <p>
+                <img alt={`${leaderboardItem.name}'s Avatar`} className="avatar" src={leaderboardItem.avatarURL} />
+              </p>
+              <p>{leaderboardItem.answerCount}</p>
+              <p>{leaderboardItem.questionCount}</p>
+              <p>{leaderboardItem.score}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function mapStateToProps({ loggedInUser, leaderboard }) {
