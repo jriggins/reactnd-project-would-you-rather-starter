1b1db740b1bc4cd7d3468dda927a78e91dbd5733
Jesse Riggins <jriggins@users.noreply.github.com>
2019-07-14 16:59:14 -0500

    Navigation functional

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class Navigation extends React.Component {
-  render() {
-    const { loggedInUser } = this.props;
-
-    return loggedInUser === null ? null : (
-      <div id="add-question-link">
-        <Link to="/">Home</Link>
-        <Link to="/add">Add Question</Link>
-        <Link to="/leaderboard">Leaderboard</Link>
-      </div>
-    );
-  }
+function Navigation({ loggedInUser }) {
+  return loggedInUser === null ? null : (
+    <div id="add-question-link">
+      <Link to="/">Home</Link>
+      <Link to="/add">Add Question</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+    </div>
+  );
 }
 
 function mapStateToProps({ loggedInUser }) {
